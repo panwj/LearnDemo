69866102d243b9e24738e4582fab076f9d4baafa
panwenjuan <18317713198@163.com>
2017-09-26 15:50:00 +0800

    add project

---
+++ MVPDemo/app/src/main/java/com/mvp/ben/Apple.java
@@ -0,0 +1,25 @@
+package com.mvp.ben;
+
+/**
+ * Created by panwenjuan on 17-9-26.
+ */
+public class Apple {
+    private String name;
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ MVPDemo/app/src/main/java/com/mvp/model/IAppleModel.java
@@ -0,0 +1,7 @@
+package com.mvp.model;
+
+/**
+ * Created by panwenjuan on 17-9-26.
+ */
+public class IAppleModel {
+}
